fbff13e06267ecc1164a7821e493f9c7384f9121
Chayan Mojo <chayan@mojowebtech.com>
2024-08-07 17:29:46 +0530

    axios aync await

--- api/index.js
+++ api/index.js
@@ -19,8 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(body_parser.json()); 
 
-const sendMessage = (sender_psid, player_id, message, title, image_url) => {
-  // Construct the message body
+
+const sendMessage = async(sender_psid, player_id, message, title, image_url) => {
+
   let request_body = {
     "recipient": {
       "id": sender_psid
@@ -50,25 +51,20 @@ const sendMessage = (sender_psid, player_id, message, title, image_url) => {
         }
       }
     }
-  }  
-
-  // console.log(process.env.PAGE_ACCESS_TOKEN)
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    console.log("Hii");
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
+  };
+  await axios.post(`https://graph.facebook.com/v16.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`, request_body, {
+    headers: {
+      'Content-Type': 'application/json'
     }
-  }); 
-  
+  })
+  .then(response => {
+    console.log('Message sent!', response.data);
+  })
+  .catch(error => {
+    console.error('Unable to send message:', error.response ? error.response.data : error.message);
+  });
 }
+
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 

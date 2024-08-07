8ee1c3f904652ead155a4f2884f0c8ed6487794b
Chayan Mojo <chayan@mojowebtech.com>
2024-08-07 17:21:51 +0530

    -

--- api/index.js
+++ api/index.js
@@ -53,32 +53,20 @@ const sendMessage = (sender_psid, player_id, message, title, image_url) => {
   }  
 
   // console.log(process.env.PAGE_ACCESS_TOKEN)
-  
-  axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`, request_body, {
-    headers: {
-      'Content-Type': 'application/json'
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    console.log("Hii");
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
     }
-  })
-  .then(response => {
-    console.log('message sent!', response.data);
-  })
-  .catch(error => {
-    console.error('Unable to send message:', error.response ? error.response.data : error.message);
-  });
-  
-  // // Send the HTTP request to the Messenger Platform
-  // request({
-  //   "uri": "https://graph.facebook.com/v2.6/me/messages",
-  //   "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-  //   "method": "POST",
-  //   "json": request_body
-  // }, (err, res, body) => {
-  //   if (!err) {
-  //     console.log('message sent!')
-  //   } else {
-  //     console.error("Unable to send message:" + err);
-  //   }
-  // }); 
+  }); 
   
 }
 
